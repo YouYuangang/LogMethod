d09b2c41661530638ce6e987cc46d800714de603
YouYuangang <17671311571@163.com>
2020-10-22 15:58:29 +0800

    在数据预处理模块，增加去除无效函数，增加曲线粗化函数

--- code/Main_dataprovider_test.py
+++ code/Main_dataprovider_test.py
@@ -4,7 +4,12 @@ from log_plot_package import simple_log_plot
 from data_helper_package import data_preprocesser
 from data_helper_package import well_segment_provider
 
-#测试rawdata_provider.RawDataProvider
+#测试示范文件
+#去掉相应的一对注释查看运行结果是否正常，熟悉接口的使用
+
+
+'''
+#测试提供原始数据的类：rawdata_provider.RawDataProvider
 #数据接口测试，入口文件
 test = rawdata_provider.RawDataProvider()
 #打印有多少口井
@@ -15,11 +20,12 @@ print("所有井的名称："+str(test.get_well_allnames()))
 AC_data = test.get_column_floatData('well116','AC')
 #获取well116井每个采样点对应的深度
 depth = test.get_well_depthdata('well116')
-#画图测试
-simple_log_plot.plot_log(depth,AC_data)
+#画图,未去除无效数据所以图有些奇怪
+simple_log_plot.plot_log(depth,AC_data,"depth","AC")
+'''
 
-
-#测试well_segment_provider.WellSegmentProvider
+'''
+#测试提供地层信息的类：well_segment_provider.WellSegmentProvider
 wellsegmentProvider = well_segment_provider.WellSegmentProvider()
 #打印well84包含哪些地层
 print("well84包含的地层："+str(wellsegmentProvider.get_segmentnames_fromwell("well84")))
@@ -31,4 +37,21 @@ print("含有石炭系地层的井："+str(wellsegmentProvider.get_wellnames_con
 longtanzu_ac_data = wellsegmentProvider.get_segment_columndata("well84","龙潭组","AC")
 #获取well84龙潭组地层的深度数据
 depth = wellsegmentProvider.get_segment_depthdata("well84","龙潭组")
-simple_log_plot.plot_log(depth,longtanzu_ac_data)
+simple_log_plot.plot_log(depth,longtanzu_ac_data,"depth","AC")
+'''
+
+
+#测试数据预处理的模块:data_helper_package.data_processer
+test = rawdata_provider.RawDataProvider()
+#获取well84井AC数据
+ac_data = test.get_column_floatData("well84","cal")
+#获取well84深度数据
+depth = test.get_well_depthdata("well84")
+#去除原始数据中的无用数据
+data_preprocesser.data_except_invalid(depth,ac_data)
+#提取关键点，曲线粗化
+depth1,data1 = data_preprocesser.getkeypoint_from_rawdata(depth,ac_data)
+print("粗化前的采样点数："+str(len(ac_data)))
+print("粗化后的采样点数："+str(len(data1)))
+#画出原始曲线和粗化后的曲线
+simple_log_plot.plot_2_log(depth1,data1,"depth","CAL",depth,ac_data,"depth","CAL")
--- code/data_helper_package/data_preprocesser.py
+++ code/data_helper_package/data_preprocesser.py
@@ -3,9 +3,57 @@ import numpy as np
 INVALID_DATA = -99999.0
 PRECISION = 0.0001
 def is_valid_data(data):
-    if(abs(data-INVALID_DATA)>PRECISION):
+    if(abs(data-INVALID_DATA)>PRECISION and data>INVALID_DATA):
         return True
     return False
+def filter_by_threepoint(data,times):
+    if len(data)<3:
+        return data
+    if times<1:
+        return data
+    for i in range(0,times):
+        for j in range(1,len(data)-1):
+            data[j]=data[j-1]*0.2+data[j]*0.6+data[j+1]*0.2
+
+def data_except_invalid(depth,data):
+    j=0
+    for i in range(len(data)):
+        if( is_valid_data(data[i])):
+            data[j], depth[j] = data[i], depth[i]
+            j+=1
+    del data[j:]
+    del depth[j:]
+
+def keep_key_point(depth,data,start,end,threshold):
+    ydata = data
+    xdata = depth
+    if(start>=end):
+        return
+    keypoint_index = -1
+    max_dist = threshold
+    k = ( ydata[end] - ydata[start] ) / ( xdata[end] - xdata[start] )
+    b = ( ydata[start] * xdata[end] - ydata[end] * xdata[start] ) / ( xdata[end] - xdata[start] )
+    for i in range(start+1,end):
+        dist = abs( k * xdata[i] + b - ydata[i] )
+        if ( dist > max_dist ):
+            max_dist, keypoint_index = dist, i
+    if( keypoint_index == -1 ):
+        for i in range(start+1,end):
+            ydata[i] = INVALID_DATA
+        return
+    keep_key_point(depth,data,start,keypoint_index,threshold)
+    keep_key_point(depth,data,keypoint_index,end,threshold)
+
+def getkeypoint_from_rawdata(depth,data):
+    depthT = depth.copy()
+    dataT = data.copy()
+    data_except_invalid(depthT,dataT)
+    filter_by_threepoint(dataT,20)
+    threshold = np.mean(dataT)/10.0
+    keep_key_point(depthT,dataT,0,len(dataT)-1,threshold)
+    data_except_invalid(depthT,dataT)
+    return depthT,dataT
+
 class DataPreprocesser:
     rawDataGetter = rawdata_provider.RawDataProvider()
     def get_column_floatdata(self,wellName,columnName):
